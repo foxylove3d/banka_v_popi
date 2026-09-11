1eb61eb64f1d065fa7a6466cad329890de3024da
Maryia Vasilkova <54509814+foxylove3d@users.noreply.github.com>
2026-09-12 01:36:17 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,39 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo, FSInputFile
+
+# Твой токен от @BotFather
+BOT_TOKEN = "8891218010:AAEsjcIe3fRb1tlX6EHHkipDV5U6UdZavMI"
+
+# Ссылка на твой GitHub Pages
+WEB_APP_URL = "https://foxylove3d.github.io/banka_v_popi/"
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def cmd_start(message: types.Message):
+    # Кнопка для запуска Mini App
+    kb = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(
+            text="присесть", 
+            web_app=WebAppInfo(url=WEB_APP_URL)
+        )]
+    ])
+    
+    # Загружаем приветственную картинку
+    photo = FSInputFile("privetstvie.jpg")
+    
+    # Отправляем фото и кнопку
+    await message.answer_photo(
+        photo=photo,
+        reply_markup=kb
+    )
+
+async def main():
+    print("Бот запущен и готов к работе!")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
